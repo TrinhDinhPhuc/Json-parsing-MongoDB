0153f253c869d48b52d6e8e20e8163f92009ed1b
HArry <phuccoi996@gmail.com>
2018-10-11 11:12:56 +0700

    done

--- main.py
+++ main.py
@@ -1,7 +1,63 @@
 import tarfile
 import time as time_import,datetime
 import os,glob,gzip,json
+from pymongo import MongoClient
+import pprint
 class knorex:
+    def __init__(self):
+        self.client = MongoClient('localhost',27017)
+        self.db = self.client['analyticsInterview']
+        self.collection = self.db['individialWins']
+        self.collection_geo = self.db['geoAggregation']
+    def mongodb(self,mylist):
+        return self.collection.insert_many(mylist)
+    def query(self):
+        pipeline = [
+            {
+                u"$group": {
+                    u"_id": {
+                        u"campaignId": u"$campaignId",
+                        u"creativeId": u"$creativeId",
+                        u"adgroupId": u"$adgroupId",
+                        u"geo": u"$geo",
+                        u"time": u"$time"
+                    },
+                    u"SUM(price)": {
+                        u"$sum": u"$price"
+                    },
+                    u"MIN(price)": {
+                        u"$min": u"$price"
+                    },
+                    u"MAX(price)": {
+                        u"$max": u"$price"
+                    }
+                }
+            },
+            {
+                u"$project": {
+                    u"_id": 0,
+                    u"campaignId": u"$_id.campaignId",
+                    u"creativeId": u"$_id.creativeId",
+                    u"adgroupId": u"$_id.adgroupId",
+                    u"geo": u"$_id.geo",
+                    u"time": u"$_id.time",
+                    u"SUM(price)": u"$SUM(price)",
+                    u"MIN(price)": u"$MIN(price)",
+                    u"MAX(price)": u"$MAX(price)"
+                }
+            }
+        ]
+
+        cursor = self.collection.aggregate(
+            pipeline,
+            allowDiskUse=True
+        )
+        try:
+            for doc in cursor:
+                self.db['geoAggregation'].insert(doc)
+        finally:
+            self.client.close()
+            print("Done!")
     def extract_tar(self,path):
         for file in glob.glob(path):
             if (tarfile.is_tarfile(file) == True):
@@ -9,8 +65,8 @@ class knorex:
                 t.extractall()
                 t.close()
     def file_content(self,file):
-        data = gzip.open(file, 'rt', encoding='utf8').readlines()
         rst = []
+        data = gzip.open(file, 'rt', encoding='utf8').readlines()
         for ele in data:
             rst.append(json.loads(ele))
         for i in range(0, len(rst), 1):
@@ -64,13 +120,28 @@ class knorex:
                 time = time_import.mktime(datetime.datetime(year, month, day, hour).timetuple())
             except KeyError:
                 time = 0
-            print(auctionId, campaignId, creativeId, adgroupId, userAgent, site, geo, exchange, price, time)
+            finally:
+                print("Done!")
+            insert_Many = []
+            insert_Many.append({"auctionId":auctionId,"campaignId": campaignId,"creativeId":creativeId, "adgroupId":adgroupId,"userAgent": userAgent,"site":site, "geo":geo, "exchange":exchange, "price":price,"time": time})
+            return self.mongodb(insert_Many)
     def get_file(self):
-        for fol in glob.glob("D:\\knorex\\raw-bid-win\\2017\\01\\11\\00\\*"):
+        for fol in glob.glob("C:\\Users\\PhucCoi\\Documents\\knorex\\ty-master\\raw-bid-win\\2017\\01\\11\\00\\*"):
             for file in glob.glob(os.path.join(fol+"\\", "*.gz")):
                 self.file_content(file)
+    def dump_out_json(self):
+        cursor = self.collection_geo.find()
+        for _document in cursor:
+            open("out.json","w+").write(str(_document))
+        return "Wrote!"
 if __name__ == '__main__':
-    path = 'D:\\knorex\\*'
-    #extract_tar(path)
     knorex = knorex()
-    knorex.get_file()
+    #EXTRACT .tar FILE
+    # path = 'C:\\Users\\PhucCoi\\Documents\\knorex\\ty-master\\*'
+    # knorex.extract_tar(path)
+    #GET CONTENT FILE
+    #knorex.get_file()
+    #INIT CONNECTION
+    #knorex.mongodb()
+    #knorex.query()
+    knorex.dump_out_json()
